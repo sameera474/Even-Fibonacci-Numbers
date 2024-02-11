793ee32319192d4a41a9bc92562b26c4ecf6caa0
Sameera Chaturanga Wagaarachchige <118694605+sameera474@users.noreply.github.com>
2024-02-11 23:47:39 +0200

    Done = 4613732

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-let seq = [0, 1];
-let seqtotal = 1;
+// let seq = [0, 1];
+// let seqtotal = 1;
 
-function fibonacciSequence(n){
+// function fibonacciSequence(n){
 
-    for (let i = 2; i < n; i++){
+//     for (let i = 2; i < n; i++){
 
 
-        let nextNumber = seq[i-1] + seq[i-2];
+//         let nextNumber = seq[i-1] + seq[i-2];
 
-        seq.push(nextNumber);
-        seqtotal += nextNumber;
+//         seq.push(nextNumber);
+//         if (nextNumber%2===0){
+//             seqtotal += nextNumber;
+//         }
+//     }
+
+//     return {seq, seqtotal};
+
+
+// }
+
+// let { seq:fibseq, seqtotal:fibtot } = fibonacciSequence(3);
+
+
+// console.log(fibtot);
+
+
+function sumEvenFibonacci(limit) {
+    let sum = 0;
+    let prev = 1;
+    let curr = 1;
+    
+    while (curr <= limit) {
+        if (curr % 2 === 0) {
+            sum += curr;
+        }
+        
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
     }
-
-    return {seq, seqtotal};
-
-
+    
+    return sum;
 }
 
-let { seq, seqtotal } = fibonacciSequence(40000000);
 
-
-console.log(seqtotal);
+console.log(sumEvenFibonacci(4000000)); 
 
